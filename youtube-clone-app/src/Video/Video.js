80607960945726666febb52d84d8f4bd51490d53
arshalsoren <arshalsoren@gmail.com>
2021-07-03 23:44:40 +0530

    update - api called/tested with search terms

--- youtube-clone-app/src/Video/Video.js
+++ youtube-clone-app/src/Video/Video.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col } from 'react-bootstrap'
 import SingleVideo from './SingleVideo'
 import Suggestions from './Suggestions'
+import Youtube from "simple-youtube-api"
+
+// Youtube API KEY
+import config from '../config'
+const youtube = new Youtube(config.api_key);
 
 function Video() {
+    const [videoList, setVideoList] = useState([]);
+
+    useEffect(
+        () => {
+            callAPI();
+        }, []
+    );
+    const callAPI = async () => {
+        const response = youtube.searchVideos('vsauce', 5);
+        console.log(response);
+
+        setVideoList(response);
+    }
+
     return (
         <React.Fragment>
             <Col xs={12} lg={8}>
-                <SingleVideo />
+                <SingleVideo detail={videoList[0]} />
             </Col>
             <Col xs={12} lg={4}>
                 <Suggestions />
