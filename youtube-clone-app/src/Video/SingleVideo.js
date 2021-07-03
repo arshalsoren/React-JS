e59e0fb6cf9a43f96b6833a58c5cab94dc715501
arshalsoren <arshalsoren@gmail.com>
2021-07-03 23:45:44 +0530

    update - search video and title, description fixed

--- youtube-clone-app/src/Video/SingleVideo.js
+++ youtube-clone-app/src/Video/SingleVideo.js
@@ -1,27 +1,25 @@
 import React from 'react'
 import { Col } from 'react-bootstrap'
 
-function SingleVideo() {
+function SingleVideo({ detail }) {
     return (
-        <>
-            <div>
+        <div>
+            {detail && (
                 <div>
                     <iframe
                         width="780"
                         height="439"
-                        src="https://www.youtube.com/embed/FxQTY-W6GIo"
+                        src={`https://www.youtube.com/embed/${detail.id}`}
                         title="YouTube video player"
-                        frameborder="0"
+                        frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen>
+                        allowFullScreen>
                     </iframe>
+                    <h1>{detail.title}</h1>
+                    <h4>{detail.description}</h4>
                 </div>
-                <div>
-                    <div>Title-Goes-Here</div>
-                    <div>Description-Here</div>
-                </div>
-            </div>
-        </>
+            )}
+        </div>
     )
 }
 
