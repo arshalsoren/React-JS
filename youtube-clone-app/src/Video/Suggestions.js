b7c0167f115088f893e832bf6d68304238ba9cfa
arshalsoren <arshalsoren@gmail.com>
2021-07-06 12:57:53 +0530

    update - load suggested videos & play-on-click suggested videos

--- youtube-clone-app/src/Video/Suggestions.js
+++ youtube-clone-app/src/Video/Suggestions.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { ListGroup } from 'react-bootstrap'
 
-function Suggestions() {
+function Suggestions({ videos, changeSelection }) {
+
+    const changeSelectedVideo = (data) => {
+        changeSelection(data)
+    }
+
     return (
         <ListGroup>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <div>
+                {videos.length > 0 && videos.map(data =>
+                    <p
+                        style={{ cursor: 'pointer' }}
+                        onClick={() => { changeSelectedVideo(data) }}
+                    >
+                        {data.title}
+                    </p>)
+                }
+            </div>
+            {/* <ListGroup.Item>Cras justo odio</ListGroup.Item>
             <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
             <ListGroup.Item>Morbi leo risus</ListGroup.Item>
             <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item> */}
         </ListGroup>
     )
 }
--- youtube-clone-app/src/Video/Video.js
+++ youtube-clone-app/src/Video/Video.js
@@ -10,6 +10,7 @@ const youtube = new Youtube(config.api_key);
 
 function Video({ searchText }) {
     const [videoList, setVideoList] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState({});
 
     useEffect(
         () => {
@@ -18,19 +19,23 @@ function Video({ searchText }) {
     );
 
     const callAPI = async () => {
-        const response = await youtube.searchVideos(searchText, 5);
+        const response = await youtube.searchVideos(searchText, 18);
         //console.log(response);
-
+        setSelectedVideo(response[0]);
         setVideoList(response);
+    }
+
+    const selectedVideoPlay=(videoDetail)=>{
+        setSelectedVideo(videoDetail);
     }
 
     return (
         <React.Fragment>
             <Col xs={12} lg={8}>
-                <SingleVideo detail={videoList[0]} />
+                <SingleVideo detail={selectedVideo} />
             </Col>
             <Col xs={12} lg={4}>
-                <Suggestions />
+                <Suggestions videos={videoList} changeSelection={selectedVideoPlay}/>
             </Col>
         </React.Fragment>
     )
